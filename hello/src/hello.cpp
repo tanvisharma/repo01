4d0b72ec202142e986ed9b294c09a8c24b66266f
Tanvi Sharma <tanvishr1197@gmail.com>
2016-06-07 15:07:31 +0530

    First commit using egit

---
+++ hello/src/hello.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : hello.cpp
+ Author      : Tanvi
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
